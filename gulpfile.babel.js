ee25f503fdf71b5d321b611ce5526e25caca0812
Leon Montealegre <leonm99@gmail.com>
2018-04-16 12:19:25 -0400

    Began editing build file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify-es').default; 
 var concat = require('gulp-concat');
 var gap = require('gulp-append-prepend');
-var browserify = require('browserify');
+var browserify = require('gulp-browserify');
 // var mocha = require('gulp-mocha');
 
 var dest = "site/app/public/js/combined.js";
 var dest_min = "site/app/public/js/combined-min.js";
+
+var dst = "site/app/public/js/Bundle.js";
+var src = "site/app/public/js/views/Main.js";
 
 var lib = ["libraries/**/*"];
 var controller = ["controllers/**/*"];
@@ -17,16 +20,12 @@ var files = lib.concat(controller, model, view);
 var paths = files.map(function(file) { return "site/app/public/js/" + file + ".js"; });
 var test_paths = files.map(function(file) { return "tests/app/public/js/" + file + ".js"; });
 
-function build() {
-    console.log(browserify);
-    
-    gulp.src(paths)
-      .pipe(concat(dest))
-      .pipe(gap.prependText("var __TESTING__ = false;\n\n"))
-      .pipe(gulp.dest("."))
-      .pipe(uglify())
-      .pipe(concat(dest_min))
-      .pipe(gulp.dest("."));
+function build() {    
+    gulp.src(src)
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(concat(dst))
+        .pipe(gulp.dest("."));
       
     gulp.src(paths.concat(test_paths))
       .pipe(concat("tests/index.js"))
